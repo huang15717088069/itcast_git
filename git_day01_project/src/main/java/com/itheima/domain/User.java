aa619e5cb3e277d945829e0a90f1b48e3dc6590e
hrf <15717088069@163.com>
2018-10-20 12:07:00 +0800

    dfdfffaswdasdasdasdasdasdasdasdasd

---
+++ git_day01_project/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,16 @@
+package com.itheima.domain;
+
+/********
+ Package:com.itheima.domain
+ Description:TODO
+ Author:huangrongfu
+ Date:Created in 2018/10/2012:04
+ Company:传智播客
+ Copyright:Copyright(c)2017
+ Version:0.0.1
+ Modified By:
+ ******/
+public class User {
+    private Integer id;
+    private String user;
+}
